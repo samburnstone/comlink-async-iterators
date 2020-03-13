a30ec7a6a3aea4497386a15e7be8f8d117d9e168
Sam Burnstone <sam.burnstone@gmail.com>
2020-03-13 17:37:32 +0000

    Ass very basic starting and stopping of counter using post message

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-const worker = new Worker("./worker.ts", { type: "module" });
+import { Messages } from "./worker/index";
 
-worker.onmessage = event => {
-  console.log(event.data);
+const worker = new Worker("./worker", { type: "module" });
+
+let isCounting = false;
+
+const buttonTitle = () => (isCounting ? "Stop" : "Start");
+
+const button = document.createElement("button");
+button.innerText = buttonTitle();
+button.onclick = () => {
+  worker.postMessage(
+    isCounting ? Messages.STOP_COUNTER : Messages.START_COUNTER
+  );
+  isCounting = !isCounting;
+  button.innerText = buttonTitle();
 };
+document.body.appendChild(button);
--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,15 +1,36 @@
+export enum Messages {
+  START_COUNTER = "START_COUNTER",
+  STOP_COUNTER = "STOP_COUNTER"
+}
+
 const sleep = () =>
   new Promise(res => {
     setTimeout(res, 1000);
   });
 
-(async () => {
+async function* counter() {
   let counter = 0;
   while (true) {
+    yield counter++;
     await sleep();
-
-    // Trick to allow us to use the Worker.postMessage rather than Window.postMessage (signature's differ, so TS will complain)
-    const ctx: Worker = self as any;
-    ctx.postMessage(counter++);
   }
-})();
+}
+
+let shouldCount = false;
+
+addEventListener("message", async e => {
+  if (e.data === Messages.START_COUNTER) {
+    shouldCount = true;
+    for await (const value of counter()) {
+      if (!shouldCount) {
+        console.log("Counter cancelled");
+        break;
+      }
+      console.log(value);
+    }
+  }
+
+  if (e.data === Messages.STOP_COUNTER) {
+    shouldCount = false;
+  }
+});
