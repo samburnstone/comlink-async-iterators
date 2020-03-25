4154ca63f9e70ca14e8207023f3ccd6a7c828b20
Sam Burnstone <sam.burnstone@gmail.com>
2020-03-25 21:40:38 +0000

    Handle calling throw method on iterator

--- src/iterableTransferHandlers.mjs
+++ src/iterableTransferHandlers.mjs
@@ -1,18 +1,26 @@
+const MESSAGE_TYPES = {
+  NEXT: "NEXT",
+  RETURN: "RETURN",
+  THROW: "THROW"
+};
+
 const listen = async (iterator, port) => {
   port.onmessage = async ({ data: { type, value } }) => {
     switch (type) {
-      case "NEXT":
+      case MESSAGE_TYPES.NEXT:
         port.postMessage(await iterator.next(value));
         break;
-      case "RETURN":
-        if (iterator.return) {
-          port.postMessage(await iterator.return(value));
+      case MESSAGE_TYPES.RETURN:
+        if (!iterator.return) {
+          throw new Error('"return" does not exist on iterator');
         }
+        port.postMessage(await iterator.return(value));
         break;
-      case "THROW":
-        if (iterator.throw) {
-          port.postMessage(await iterator.throw(value));
+      case MESSAGE_TYPES.THROW:
+        if (!iterator.throw) {
+          throw new Error('"throw" does not exist on iterator');
         }
+        port.postMessage(await iterator.throw(value));
     }
   };
 };
@@ -27,46 +35,49 @@ const makeTransferHandler = symbolLookup => ({
     return [port2, [port2]];
   },
   deserialize: port => {
-    async function* generatorFn() {
-      const read = () =>
-        new Promise(resolve => {
-          port.onmessage = ({ data }) => {
-            resolve(data);
-          };
-        });
+    const nextIteratorResult = () =>
+      new Promise(resolve => {
+        port.onmessage = ({ data }) => {
+          resolve(data);
+        };
+      });
 
-      try {
-        while (true) {
-          // TODO: is "done" the best way to observe this?
-          const { value, done } = await read();
-          if (done) {
-            break;
-          }
-          yield value;
+    async function* generatorFn() {
+      while (true) {
+        // TODO: is "done" the best way to observe this?
+        const { value, done } = await nextIteratorResult();
+        if (done) {
+          break;
         }
-      } finally {
-        // TODO: workout if this necessary... might be needed to force clean-up of message channels
-        port.close();
+        yield value;
       }
     }
 
     const generator = generatorFn();
 
+    // Cover the case where a user   wants to be able to manually call the iterator methods
     const iterator = {
       next: value => {
-        port.postMessage({ type: "NEXT", value });
+        port.postMessage({ type: MESSAGE_TYPES.NEXT, value });
         return generator.next();
       },
       return: value => {
-        port.postMessage({ type: "RETURN", value });
-        return generator.return();
+        port.postMessage({ type: MESSAGE_TYPES.RETURN, value });
+        generator.return(value);
+        return nextIteratorResult();
       },
-      throw: value => {
-        port.postMessage({ type: "ERROR", value });
-        return generator.throw();
+      throw: async value => {
+        port.postMessage({ type: MESSAGE_TYPES.THROW, value });
+        const result = await nextIteratorResult();
+        if (result.done) {
+          return generator.throw(value);
+        }
+        // Error caught by iterator, return result
+        return result;
       }
     };
 
+    // Make it iterable so it can be used in for-await-of statement
     iterator[Symbol.asyncIterator] = () => iterator;
 
     return iterator;
