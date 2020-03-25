f011efcfaff9c86e3c77a2aae5e904d0ce948268
Sam Burnstone <sam.burnstone@gmail.com>
2020-03-25 22:26:43 +0000

    Only call throw and return if defined
    Realised when doing this, there was no need for the intermediate
    generator function, so this has been removed

--- src/iterableTransferHandlers.mjs
+++ src/iterableTransferHandlers.mjs
@@ -1,7 +1,8 @@
 const MESSAGE_TYPES = {
   NEXT: "NEXT",
   RETURN: "RETURN",
-  THROW: "THROW"
+  THROW: "THROW",
+  DEFINED_OPTIONAL_ITERATOR_FNS: "DEFINED_OPTIONAL_ITERATOR_FNS"
 };
 
 const listen = async (iterator, port) => {
@@ -11,15 +12,9 @@ const listen = async (iterator, port) => {
         port.postMessage(await iterator.next(value));
         break;
       case MESSAGE_TYPES.RETURN:
-        if (!iterator.return) {
-          throw new Error('"return" does not exist on iterator');
-        }
         port.postMessage(await iterator.return(value));
         break;
       case MESSAGE_TYPES.THROW:
-        if (!iterator.throw) {
-          throw new Error('"throw" does not exist on iterator');
-        }
         port.postMessage(await iterator.throw(value));
     }
   };
@@ -31,51 +26,56 @@ const makeTransferHandler = symbolLookup => ({
   },
   serialize: iterable => {
     const { port1, port2 } = new MessageChannel();
-    listen(iterable[symbolLookup](), port1);
+
+    const iterator = iterable[symbolLookup]();
+
+    const definedOptionalIteratorFns = {
+      throw: !!iterator.throw,
+      return: !!iterator.return
+    };
+
+    port1.postMessage({
+      type: MESSAGE_TYPES.DEFINED_OPTIONAL_ITERATOR_FNS,
+      value: definedOptionalIteratorFns
+    });
+
+    listen(iterator, port1);
     return [port2, [port2]];
   },
-  deserialize: port => {
-    const nextIteratorResult = () =>
+  deserialize: async port => {
+    const nextPortMessage = () =>
       new Promise(resolve => {
         port.onmessage = ({ data }) => {
           resolve(data);
         };
       });
 
-    async function* generatorFn() {
-      while (true) {
-        // TODO: is "done" the best way to observe this?
-        const { value, done } = await nextIteratorResult();
-        if (done) {
-          break;
-        }
-        yield value;
-      }
-    }
+    // First message to come back will advertise functions defined on iterator
+    const { value: availableIteratorFns } = await nextPortMessage();
 
-    const generator = generatorFn();
-
-    // Cover the case where a user   wants to be able to manually call the iterator methods
+    // Cover the case where a user wants to be able to manually call the iterator methods
     const iterator = {
       next: value => {
         port.postMessage({ type: MESSAGE_TYPES.NEXT, value });
-        return generator.next();
-      },
-      return: value => {
-        port.postMessage({ type: MESSAGE_TYPES.RETURN, value });
-        generator.return(value);
-        return nextIteratorResult();
-      },
-      throw: async value => {
-        port.postMessage({ type: MESSAGE_TYPES.THROW, value });
-        const result = await nextIteratorResult();
-        if (result.done) {
-          return generator.throw(value);
-        }
-        // Error caught by iterator, return result
-        return result;
+        return nextPortMessage();
       }
     };
+
+    // return and throw functions are optional (https://tc39.es/ecma262/#table-async-iterator-optional),
+    // so we check they're available
+    if (availableIteratorFns.return) {
+      iterator.return = value => {
+        port.postMessage({ type: MESSAGE_TYPES.RETURN, value });
+        return nextPortMessage();
+      };
+    }
+
+    if (availableIteratorFns.throw) {
+      iterator.throw = async value => {
+        port.postMessage({ type: MESSAGE_TYPES.THROW, value });
+        return nextPortMessage();
+      };
+    }
 
     // Make it iterable so it can be used in for-await-of statement
     iterator[Symbol.asyncIterator] = () => iterator;
