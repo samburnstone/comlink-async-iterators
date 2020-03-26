67dbd3256dc953b41289943daeca465385e87728
Sam Burnstone <sam.burnstone@gmail.com>
2020-03-26 14:45:04 +0000

    Add example of providing values to iterator

---
+++ examples/generator-providing-values/index.mjs
@@ -0,0 +1,20 @@
+import {
+  wrap,
+  transferHandlers
+} from "../../node_modules/comlink/dist/esm/comlink.mjs";
+import { iteratorTransferHandler } from "../../../src/iterableTransferHandlers.mjs";
+
+transferHandlers.set("iterator", iteratorTransferHandler);
+
+const worker = wrap(new Worker("./worker/index.mjs", { type: "module" }));
+
+(async () => {
+  const famousPeopleGenerator = await worker.createFamousPeopleGenerator();
+
+  // First time calling next arg will be ignored (returns Pope as value)
+  console.log(await famousPeopleGenerator.next());
+  // While are arg is referring to the pope, the value being returned is Victoria Beckham
+  console.log(await famousPeopleGenerator.next("Don't know him that well"));
+  console.log(await famousPeopleGenerator.next("Not a fan..."));
+  console.log(await famousPeopleGenerator.next("Almost forgot about him!"));
+})();
---
+++ examples/generator-providing-values/worker/index.mjs
@@ -0,0 +1,21 @@
+import {
+  expose,
+  transferHandlers
+} from "../../../node_modules/comlink/dist/esm/comlink.mjs";
+import { iteratorTransferHandler } from "../../../src/iterableTransferHandlers.mjs";
+
+transferHandlers.set("iterator", iteratorTransferHandler);
+
+function* createFamousPeopleGenerator() {
+  const famousPeople = ["The Pope", "Victoria Beckham", "Michael Owen"];
+  for (const person of famousPeople) {
+    const verdict = yield person;
+    console.log(`Verdict on ${person}: ${verdict}`);
+  }
+}
+
+const exports = {
+  createFamousPeopleGenerator
+};
+
+expose(exports);
